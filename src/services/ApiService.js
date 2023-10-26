ad55a197498f0cf69e2d9d0dfca647af05eb1062
Helio Leao <hmtleao@hotmail.com>
2023-10-26 10:17:05 -0300

    create request token and apiservice refactoring

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,12 +1,17 @@
 import axios from "axios";
 
+
+// CONSTANTS
+
 const { API_TOKEN } = process.env;
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original';
 
-const generalQuery = `&include_adult=false&language=pt-BR`;
+const GENERAL_QUERY = `&include_adult=false&language=pt-BR`;
 
+
+// ENUMS
 
 const TrendingTimeWindow = {
   DAY: 'day',
@@ -36,34 +41,36 @@ const TvShowStatus = {
 }
 
 
+// MEDIA FETCH FUNCTIONS
+
 async function fetchPopularMovies(page = 1) {
-  return await fetchData(`/movie/popular`, `page=${page}` + generalQuery);
+  return await fetchData(`/movie/popular`, `page=${page}` + GENERAL_QUERY);
 }
 
 async function fetchPopularTvShows(page = 1) {
-  return await fetchData(`/tv/popular`, `page=${page}` + generalQuery);
+  return await fetchData(`/tv/popular`, `page=${page}` + GENERAL_QUERY);
 }
 
 async function fetchTrendingMovies(timeWindow = TrendingTimeWindow.DAY) {
-  return await fetchData(`/trending/movie/${timeWindow}`, generalQuery);
+  return await fetchData(`/trending/movie/${timeWindow}`, GENERAL_QUERY);
 }
 
 async function fetchTrendingTvShows(timeWindow = TrendingTimeWindow.DAY) {
-  return await fetchData(`/trending/tv/${timeWindow}`, generalQuery);
+  return await fetchData(`/trending/tv/${timeWindow}`, GENERAL_QUERY);
 }
 
 async function fetchMovieDetails(movieId) {
   return await fetchData(`/movie/${movieId}`,
-    `append_to_response=recommendations,credits` + generalQuery);
+    `append_to_response=recommendations,credits` + GENERAL_QUERY);
 }
 
 async function fetchTvShowDetails(tvShowId) {
   return await fetchData(`/tv/${tvShowId}`,
-    `append_to_response=recommendations,credits` + generalQuery);
+    `append_to_response=recommendations,credits` + GENERAL_QUERY);
 }
 
 async function fetchMulti(name = '', page = 1) {
-  return await fetchData(`/search/multi`, `query=${name}&page=${page}` + generalQuery);
+  return await fetchData(`/search/multi`, `query=${name}&page=${page}` + GENERAL_QUERY);
 }
 
 // NOTE: removed genre is documentary (id 99)
@@ -71,7 +78,7 @@ async function fetchMoviesWithPeople(peopleIds = [], page = 1) {
   return await fetchData(
     `/discover/movie`,
     `include_video=false&page=${page}&sort_by=primary_release_date.desc&with_people=${
-      peopleIds.join('|')}&without_genres=99` + generalQuery,
+      peopleIds.join('|')}&without_genres=99` + GENERAL_QUERY,
   );
 }
 
@@ -79,11 +86,14 @@ function fetchFullImagePath(imagePath) {
   return imagePath ? IMAGE_BASE_URL + imagePath : null;
 }
 
+
+// HELPER FUNCTIONS
+
 async function fetchData(endpoint, query) {
   const headers = {
     Authorization: `Bearer ${API_TOKEN}`
   };
-  let url = `${API_BASE_URL + endpoint}`;
+  let url = API_BASE_URL + endpoint;
 
   if(query) {
     url += `?${query}`;
@@ -92,6 +102,13 @@ async function fetchData(endpoint, query) {
 
   const response = await axios.get(url, {headers});
   return response.data;
+}
+
+
+// AUTHENTICATION FUNCTIONS
+
+async function createRequestToken() {
+  return await fetchData('/authentication/token/new');
 }
 
 
@@ -109,4 +126,5 @@ export default {
   fetchMulti,
   fetchMoviesWithPeople,
   fetchFullImagePath,
+  createRequestToken,
 }
