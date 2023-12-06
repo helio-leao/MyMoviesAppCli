85a29ab6ea82eb08d51176a265b9de6742b135fd
Helio Leao <hmtleao@hotmail.com>
2023-12-05 22:23:47 -0300

    media details screen refactoring

--- src/components/MediaDetails.js
+++ src/components/MediaDetails.js
@@ -11,177 +11,141 @@ import CollapsibleText from '../components/CollapsibleText';
 import LoadableImage from '../components/LoadableImage';
 
 
-export default function MediaDetails({mediaId, mediaType}) {
+export default function MediaDetails({mediaDetails}) {
 
-    const {session} = useContext(SessionContext);
-    const [isLoading, setIsLoading] = useState(true);
-    const [mediaData, setMediaData] = useState(null);
+  const {session} = useContext(SessionContext);
   
   
-    useEffect(() => {
-      async function loadMediaData() {
-  
-        try {
-          switch(mediaType) {
-            case ApiService.MediaType.MOVIE:
-              const movieDetails = await ApiService.fetchMovieDetails(mediaId);
-              setMediaData(movieDetails);
-              break;
-            case ApiService.MediaType.TV:
-              const tvShowDetails = await ApiService.fetchTvShowDetails(mediaId);
-              setMediaData(tvShowDetails);
-              break;
-            default:
-              console.warn('Media type invalid:', mediaType);
-          }
-          setIsLoading(false);
-        } catch (error) {
-          console.error(error);
-          ToastAndroid.show('Ocorreu um erro.', ToastAndroid.SHORT);
-        }
-      }
-      loadMediaData();
-    }, []);
-  
-  
-    async function onFavoritePress() {
-      try {
-        const response = await ApiService.addFavorite(
-          session.user.id,
-          session.id,
-          mediaData,
-          mediaType,
-        );
-  
-        if(response.success) {
-          ToastAndroid.show('Operação bem sucedida.', ToastAndroid.SHORT);
-        }
-      } catch (error) {
-        console.log('error response: ', error.response.data);
-        ToastAndroid.show('Ocorreu um erro.', ToastAndroid.SHORT);
-      }
-    }
-  
-  
-    if(isLoading) {
-      return (
-        <View style={[styles.container, {justifyContent: 'center'}]}>
-          <ActivityIndicator color={'white'} size={'large'} />
-        </View>
+  async function onFavoritePress() {
+    try {
+      const response = await ApiService.addFavorite(
+        session.user.id,
+        session.id,
+        mediaDetails,
+        mediaType,
       );
+
+      if(response.success) {
+        ToastAndroid.show('Operação bem sucedida.', ToastAndroid.SHORT);
+      }
+    } catch (error) {
+      console.log('error response: ', error.response.data);
+      ToastAndroid.show('Ocorreu um erro.', ToastAndroid.SHORT);
     }
-  
-    return (
-      <View style={styles.container}>
-        {/* image */}
-        <LoadableImage
-          style={styles.backdropImage}
-          source={{uri: ApiService.fetchFullImagePath(mediaData?.backdrop_path)}}
-          placeholder={placeholder_poster}
-        />
-        
-        {/* gradient container */}
-        <LinearGradient
-          style={styles.gradientContainer}
-          colors={['#111', '#11111100']}
-          start={{x: 0, y: 0.15}} end={{x: 0, y: 0}}
-        >
-          {/* title for movies or name for tv */}
-          <Text style={styles.title} numberOfLines={2}>
-            {mediaData?.title || mediaData?.name}
-          </Text>
-          
-  
-          <ScrollView>          
-            <View style={styles.contentScrollContainer}>
-  
-              {/* genres */}
-              <View>
-                <ScrollView showsHorizontalScrollIndicator={false} horizontal>
-                  <View style={styles.genresContainer}>
-                    {mediaData?.genres.map(genre => (
-                      <View key={genre.id} style={styles.genrePill}>
-                        <Text style={styles.genreText}>{genre.name}</Text>
-                      </View>
-                    ))}
-                  </View>
-                </ScrollView>
-              </View>
-  
-              {/* ratings and favorite button */}
-              <View style={styles.ratingsAndFavoriteContainer}>
-                <View style={styles.ratingsContainer}>
-                  <Fontisto name="star" size={30} color="yellow" />
-  
-                  <View>
-                    {/* NOTE: good way to use text with multiple styles */}
-                    <Text style={{color: '#fff'}}>
-                      <Text style={{fontSize: 20, fontWeight: '800'}}>
-                        {mediaData?.vote_average.toFixed(1)}
-                      </Text>
-                      <Text style={{fontSize: 18}}>
-                        /10
-                      </Text>
-                    </Text>
-  
-                    <Text style={styles.contentText}>{mediaData?.vote_count}</Text>
-                  </View>
-                </View>
-  
-                {session?.user && (
-                  <TouchableOpacity style={styles.favoriteButton} onPress={onFavoritePress}>
-                    <FontAwesome name="heart" size={20} color="white" />
-                    <Text style={styles.favoriteButtonText}>Favoritar</Text>
-                  </TouchableOpacity>
-                )}
-              </View>
-              {/* end ratings and favorite button */}
-  
-              <MediaContent mediaData={mediaData} />
-  
-              {mediaData?.credits.cast.length > 0 && (
-                <View style={{marginTop: 30}}>
-                  <Text style={[styles.contentText, {fontSize: 22, marginHorizontal: 10, marginBottom: 16}]}>
-                    Elenco
-                  </Text>
-                  <MediaRowList
-                    mediaData={mediaData.credits.cast}
-                    contentContainerStyle={{paddingHorizontal: 10}}
-                  />
-                </View>
-              )}
-  
-              {mediaData?.credits.crew.length > 0 && (
-                <View style={{marginTop: 30}}>
-                  <Text style={[styles.contentText, {fontSize: 22, marginHorizontal: 10, marginBottom: 16}]}>
-                    Produção
-                  </Text>
-                  <MediaRowList
-                    mediaData={mediaData.credits.crew}
-                    contentContainerStyle={{paddingHorizontal: 10}}
-                  />
-                </View>
-              )}
-  
-              {mediaData?.recommendations.total_results > 0 && (
-                <View style={{marginTop: 30}}>
-                  <Text style={[styles.contentText, {fontSize: 22, marginHorizontal: 10, marginBottom: 16}]}>
-                    Recomendações
-                  </Text>
-                  <MediaRowList
-                    mediaData={mediaData.recommendations.results}
-                    contentContainerStyle={{paddingHorizontal: 10}}
-                  />
-                </View>
-              )}
-  
-            </View>
-          </ScrollView>
-          
-        </LinearGradient>
-      </View>
-    );
   }
+
+
+  return (
+    <View style={styles.container}>
+      {/* image */}
+      <LoadableImage
+        style={styles.backdropImage}
+        source={{uri: ApiService.fetchFullImagePath(mediaDetails?.backdrop_path)}}
+        placeholder={placeholder_poster}
+      />
+      
+      {/* gradient container */}
+      <LinearGradient
+        style={styles.gradientContainer}
+        colors={['#111', '#11111100']}
+        start={{x: 0, y: 0.15}} end={{x: 0, y: 0}}
+      >
+        {/* title for movies or name for tv */}
+        <Text style={styles.title} numberOfLines={2}>
+          {mediaDetails?.title || mediaDetails?.name}
+        </Text>
+        
+
+        <ScrollView>          
+          <View style={styles.contentScrollContainer}>
+
+            {/* genres */}
+            <View>
+              <ScrollView showsHorizontalScrollIndicator={false} horizontal>
+                <View style={styles.genresContainer}>
+                  {mediaDetails?.genres.map(genre => (
+                    <View key={genre.id} style={styles.genrePill}>
+                      <Text style={styles.genreText}>{genre.name}</Text>
+                    </View>
+                  ))}
+                </View>
+              </ScrollView>
+            </View>
+
+            {/* ratings and favorite button */}
+            <View style={styles.ratingsAndFavoriteContainer}>
+              <View style={styles.ratingsContainer}>
+                <Fontisto name="star" size={30} color="yellow" />
+
+                <View>
+                  {/* NOTE: good way to use text with multiple styles */}
+                  <Text style={{color: '#fff'}}>
+                    <Text style={{fontSize: 20, fontWeight: '800'}}>
+                      {mediaDetails?.vote_average.toFixed(1)}
+                    </Text>
+                    <Text style={{fontSize: 18}}>
+                      /10
+                    </Text>
+                  </Text>
+
+                  <Text style={styles.contentText}>{mediaDetails?.vote_count}</Text>
+                </View>
+              </View>
+
+              {session?.user && (
+                <TouchableOpacity style={styles.favoriteButton} onPress={onFavoritePress}>
+                  <FontAwesome name="heart" size={20} color="white" />
+                  <Text style={styles.favoriteButtonText}>Favoritar</Text>
+                </TouchableOpacity>
+              )}
+            </View>
+            {/* end ratings and favorite button */}
+
+            <MediaContent mediaData={mediaDetails} />
+
+            {mediaDetails?.credits.cast.length > 0 && (
+              <View style={{marginTop: 30}}>
+                <Text style={[styles.contentText, {fontSize: 22, marginHorizontal: 10, marginBottom: 16}]}>
+                  Elenco
+                </Text>
+                <MediaRowList
+                  mediaData={mediaDetails.credits.cast}
+                  contentContainerStyle={{paddingHorizontal: 10}}
+                />
+              </View>
+            )}
+
+            {mediaDetails?.credits.crew.length > 0 && (
+              <View style={{marginTop: 30}}>
+                <Text style={[styles.contentText, {fontSize: 22, marginHorizontal: 10, marginBottom: 16}]}>
+                  Produção
+                </Text>
+                <MediaRowList
+                  mediaData={mediaDetails.credits.crew}
+                  contentContainerStyle={{paddingHorizontal: 10}}
+                />
+              </View>
+            )}
+
+            {mediaDetails?.recommendations.total_results > 0 && (
+              <View style={{marginTop: 30}}>
+                <Text style={[styles.contentText, {fontSize: 22, marginHorizontal: 10, marginBottom: 16}]}>
+                  Recomendações
+                </Text>
+                <MediaRowList
+                  mediaData={mediaDetails.recommendations.results}
+                  contentContainerStyle={{paddingHorizontal: 10}}
+                />
+              </View>
+            )}
+
+          </View>
+        </ScrollView>
+        
+      </LinearGradient>
+    </View>
+  );
+}
   
   
   function MediaContent({mediaData}) {
@@ -313,81 +277,81 @@ export default function MediaDetails({mediaId, mediaType}) {
   }
   
   
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#111',
-    },
-    backdropImage: {
-      aspectRatio: 16/9,
-      width: '100%',
-      height: undefined,
-    },
-    gradientContainer: {
-      flex: 1,
-      marginTop: -100,
-    },
-    title: {
-      marginTop: 40,
-      fontSize: 40,
-      color: '#fff',
-      marginHorizontal: 10,
-      marginBottom: 10,
-    },
-    genresContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      paddingHorizontal: 10,
-      paddingVertical: 10,
-      marginBottom: 10,
-      gap: 8,
-    },
-    genrePill: {
-      backgroundColor: '#fff',
-      justifyContent: 'center',
-      height: 26,
-      paddingHorizontal: 12,
-      borderRadius: 12,
-    },
-    genreText: { 
-      fontSize: 14,
-      color: '#000'
-    },
-    ratingsAndFavoriteContainer: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      marginHorizontal: 10,
-      marginBottom: 20,
-    },
-    ratingsContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      gap: 16,
-    },
-    favoriteButton: {
-      paddingVertical: 10,
-      paddingHorizontal: 12,
-      flexDirection: 'row',
-      alignItems: 'center',
-      gap: 8,
-    },
-    contentContainer: {
-      marginHorizontal: 10,
-    },
-    favoriteButtonText: {
-      color: '#fff',
-    },
-    contentScrollContainer: {
-      marginBottom: 20,
-    },
-    contentText: {
-      fontSize: 18,
-      color: '#fff',
-    },
-    recommendationsRowTitle: {
-      color: '#fff',
-      fontSize: 20,
-      marginBottom: 10,
-    },
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#111',
+  },
+  backdropImage: {
+    aspectRatio: 16/9,
+    width: '100%',
+    height: undefined,
+  },
+  gradientContainer: {
+    flex: 1,
+    marginTop: -100,
+  },
+  title: {
+    marginTop: 40,
+    fontSize: 40,
+    color: '#fff',
+    marginHorizontal: 10,
+    marginBottom: 10,
+  },
+  genresContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    marginBottom: 10,
+    gap: 8,
+  },
+  genrePill: {
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    height: 26,
+    paddingHorizontal: 12,
+    borderRadius: 12,
+  },
+  genreText: { 
+    fontSize: 14,
+    color: '#000'
+  },
+  ratingsAndFavoriteContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 10,
+    marginBottom: 20,
+  },
+  ratingsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 16,
+  },
+  favoriteButton: {
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  contentContainer: {
+    marginHorizontal: 10,
+  },
+  favoriteButtonText: {
+    color: '#fff',
+  },
+  contentScrollContainer: {
+    marginBottom: 20,
+  },
+  contentText: {
+    fontSize: 18,
+    color: '#fff',
+  },
+  recommendationsRowTitle: {
+    color: '#fff',
+    fontSize: 20,
+    marginBottom: 10,
+  },
+});
--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,9 +1,127 @@
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, ToastAndroid, ActivityIndicator } from 'react-native';
+import { useContext, useEffect, useState } from 'react';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import LinearGradient from 'react-native-linear-gradient';
 import ApiService from '../services/ApiService';
+import placeholder_poster from '../assets/images/placeholder_poster.png';
+import MediaRowList from '../components/MediaRowList';
+import { SessionContext } from '../contexts/SessionContext';
+import CollapsibleText from '../components/CollapsibleText';
+import LoadableImage from '../components/LoadableImage';
 import MediaDetails from './MediaDetails';
 
 
-export default function MovieDetails({movieId}) {  
+export default function MovieDetails({movieId}) {
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [movieDetails, setMovieDetails] = useState(null);
+
+
+  useEffect(() => {
+    async function loadMovieDetails() {
+      try {
+        const movieDetails = await ApiService.fetchMovieDetails(movieId);
+        setMovieDetails(movieDetails);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+        ToastAndroid.show('Ocorreu um erro.', ToastAndroid.SHORT);
+      }
+    }
+    loadMovieDetails();
+  }, []);
+
+
+  if(isLoading) {
+    return (
+      <View style={[styles.container, {justifyContent: 'center'}]}>
+        <ActivityIndicator color={'white'} size={'large'} />
+      </View>
+    );
+  }
+
   return (
-    <MediaDetails mediaId={movieId} mediaType={ApiService.MediaType.MOVIE} />
+    <MediaDetails mediaDetails={movieDetails} />
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#111',
+  },
+  backdropImage: {
+    aspectRatio: 16/9,
+    width: '100%',
+    height: undefined,
+  },
+  gradientContainer: {
+    flex: 1,
+    marginTop: -100,
+  },
+  title: {
+    marginTop: 40,
+    fontSize: 40,
+    color: '#fff',
+    marginHorizontal: 10,
+    marginBottom: 10,
+  },
+  genresContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    marginBottom: 10,
+    gap: 8,
+  },
+  genrePill: {
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    height: 26,
+    paddingHorizontal: 12,
+    borderRadius: 12,
+  },
+  genreText: { 
+    fontSize: 14,
+    color: '#000'
+  },
+  ratingsAndFavoriteContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 10,
+    marginBottom: 20,
+  },
+  ratingsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 16,
+  },
+  favoriteButton: {
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  contentContainer: {
+    marginHorizontal: 10,
+  },
+  favoriteButtonText: {
+    color: '#fff',
+  },
+  contentScrollContainer: {
+    marginBottom: 20,
+  },
+  contentText: {
+    fontSize: 18,
+    color: '#fff',
+  },
+  recommendationsRowTitle: {
+    color: '#fff',
+    fontSize: 20,
+    marginBottom: 10,
+  },
+});
